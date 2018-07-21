1ef0a8adef8e0709889cbb0483878e29f36ad779
Xhacker's New mac <pengchuan@loocall.com>
2018-07-21 15:01:43 +0800

    fix

--- gen_temp_neo_contract.sh
+++ gen_temp_neo_contract.sh
@@ -1,5 +1,5 @@
 
-neo_workspace_path="/Users/Xhacker/Documents/DemoProjects"
+neo_workspace_path="/home/ubuntu/neo_workspace"
 cd $neo_workspace_path
 echo "Start create NEO Contract Project $1"
 temp_project_folder="neo_temp_project_$1"
