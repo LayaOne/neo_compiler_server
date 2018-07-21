63901dc610e234d1a6df8068cca766308e8db858
Xhacker's New mac <pengchuan@loocall.com>
2018-07-21 14:59:15 +0800

    fix

--- gen_temp_neo_contract.sh
+++ gen_temp_neo_contract.sh
@@ -1,9 +1,10 @@
 
-neo_workspace_path = "/home/ubuntu/neo_workspace"
+neo_workspace_path="/Users/Xhacker/Documents/DemoProjects"
 cd $neo_workspace_path
 echo "Start create NEO Contract Project $1"
-temp_project_folder = "neo_temp_project_$1"
+temp_project_folder="neo_temp_project_$1"
 final_folder_path=${neo_workspace_path}${temp_project_folder}
+echo $final_folder_path
 mkdir -p $final_folder_path
 cd $final_folder_path
 dotnet new classlib
