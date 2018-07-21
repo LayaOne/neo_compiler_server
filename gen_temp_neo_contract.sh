5dff46904a3cbeb69ed44e7d85a807352fd1da51
Xhacker's New mac <pengchuan@loocall.com>
2018-07-21 15:12:52 +0800

    fix

--- gen_temp_neo_contract.sh
+++ gen_temp_neo_contract.sh
@@ -9,4 +9,12 @@ mkdir -p $final_folder_path
 cd $final_folder_path
 dotnet new classlib
 rm ./Class1.cs
+dotnet add package Neo.SmartContract.Framework --version 2.5.4
 echo $final_folder_path" Project created"
+
+# 将文件写入拷贝到项目文件夹
+cp $2 $final_folder_path"/"
+# 编译
+dotnet publish -o ../testlib
+echo "public "${temp_project_folder}".dll"
+
